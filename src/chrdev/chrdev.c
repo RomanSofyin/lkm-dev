edf82f580a93f8700d334e7d44724e4f9f408d1f
Roman Sofyin <roma.sofin@gmil.com>
2022-08-11 17:38:19 +0300

    Filling up chrdev.c

--- src/chrdev/chrdev.c
+++ src/chrdev/chrdev.c
@@ -9,16 +9,54 @@
 
 static char *kbuf;
 static dev_t first;
-static unsigned int coont = 1;
+static unsigned int count = 1;
 static int my_major = 700, my_minor = 0;
 static struct cdev *my_cdev;
 
 #define MYDEV_NAME "mychrdev"
-#define KBUF_SIZE size_t ((10) * PAGE_SIZE)
+#define KBUF_SIZE (size_t)((10) * PAGE_SIZE)
 
 static int __init init_chrdev(void)
 {
     printk(KERN_INFO "Hello, loading");
+    kbuf = kmalloc(KBUF_SIZE, GFP_KERNEL);
+
+    first = MKDEV(my_major, my_minor);
+    register_chrdev_region(first, count, MYDEV_NAME);
+
+    my_cdev = cdev_alloc();
+
+    cdev_init(my_cdev, &mycdev_fops); // file operations instance
+    cdev_add(my_cdev, first, count);
+
+    return 0;
+}
+
+static int mychrdev_open(struct inode *inode, struct file *file)
+{
+    printk(KERN_INFO "Opening device %s:\n\n", MYDEV_NAME);
+    return 0;
+}
+
+static int mychrdev_release(struct inode *inode, struct file *file)
+{
+    printk(KERN_INFO "Releasing device %s:\n\n", MYDEV_NAME);
+    return 0;
+}
+
+static ssize_t mychrdev_read(struct file *file,
+                             char __user *buf,          // parm came from user space
+                             size_t lbuf, loff_t *ppos) // long offset
+{
+    printk(KERN_INFO "Reading device %s:\n\n", MYDEV_NAME);
+    return 0;
+}
+
+static ssize_t __exit mychrdev_write(struct file *file,
+                                     char __user *buf, // parm came from user space
+                                     size_t lbuf, loff_t *ppos)
+{
+    printk(KERN_INFO "Writing device %s:\n\n", MYDEV_NAME);
     return 0;
 }
 
