961e67986a870104227db2bff794eee1c2e0899d
Roman Sofyin <roma.sofin@gmil.com>
2022-08-12 14:48:48 +0300

    Add funcs implementation

--- src/chrdev/chrdev.c
+++ src/chrdev/chrdev.c
@@ -16,6 +16,56 @@ static struct cdev *my_cdev;
 #define MYDEV_NAME "mychrdev"
 #define KBUF_SIZE (size_t)((10) * PAGE_SIZE)
 
+
+static int mychrdev_open(struct inode *inode, struct file *file)
+{
+    printk(KERN_INFO "Opening device %s\n\n", MYDEV_NAME);
+    return 0;
+}
+
+static int mychrdev_release(struct inode *inode, struct file *file)
+{
+    printk(KERN_INFO "Releasing device %s\n\n", MYDEV_NAME);
+    return 0;
+}
+
+static ssize_t mychrdev_read(struct file *file,
+                             char __user *buf,          // parm came from user space
+                             size_t lbuf, loff_t *ppos) // long offset
+{
+    ssize_t nbytes = lbuf - copy_to_user(buf, kbuf+ *ppos, lbuf);   // copy to user space storage
+    *ppos += nbytes;
+
+    printk(KERN_INFO "Reading device %s - nbytes = %ld, ppos = %lld\n\n", MYDEV_NAME, nbytes, *ppos);
+
+    return nbytes;
+}
+
+static ssize_t __exit mychrdev_write(struct file *file,
+                                     const char __user *buf, // parm came from user space
+                                     size_t lbuf, loff_t *ppos)
+{
+    ssize_t nbytes = lbuf - copy_from_user(kbuf + *ppos, buf, lbuf);
+    *ppos += nbytes;
+ 
+    printk(KERN_INFO "Writing device %s - nbytes = %ld, ppos = %lld \n\n", MYDEV_NAME, nbytes, *ppos);
+ 
+    return nbytes;
+}
+
+static void __exit cleanup_chrdev(void)
+{
+    printk(KERN_INFO "Leaving");
+}
+
+static const struct file_operations mycdev_fops = {
+    .owner = THIS_MODULE,
+    .read = mychrdev_read,
+    .write = mychrdev_write,
+    .open = mychrdev_open,
+    .release = mychrdev_release
+};
+
 static int __init init_chrdev(void)
 {
     printk(KERN_INFO "Hello, loading");
@@ -30,39 +80,6 @@ static int __init init_chrdev(void)
     cdev_add(my_cdev, first, count);
 
     return 0;
-}
-
-static int mychrdev_open(struct inode *inode, struct file *file)
-{
-    printk(KERN_INFO "Opening device %s:\n\n", MYDEV_NAME);
-    return 0;
-}
-
-static int mychrdev_release(struct inode *inode, struct file *file)
-{
-    printk(KERN_INFO "Releasing device %s:\n\n", MYDEV_NAME);
-    return 0;
-}
-
-static ssize_t mychrdev_read(struct file *file,
-                             char __user *buf,          // parm came from user space
-                             size_t lbuf, loff_t *ppos) // long offset
-{
-    printk(KERN_INFO "Reading device %s:\n\n", MYDEV_NAME);
-    return 0;
-}
-
-static ssize_t __exit mychrdev_write(struct file *file,
-                                     char __user *buf, // parm came from user space
-                                     size_t lbuf, loff_t *ppos)
-{
-    printk(KERN_INFO "Writing device %s:\n\n", MYDEV_NAME);
-    return 0;
-}
-
-static void __exit cleanup_chrdev(void)
-{
-    printk(KERN_INFO "Leaving");
 }
 
 module_init(init_chrdev);
